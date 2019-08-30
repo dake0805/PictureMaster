1cc524792903c2cbe04ca96bc60ba745c35edd1d
jun0315 <1072505283@qq.com>
2019-08-30 18:03:56 +0800

    上次commit是增加图片库

--- app/src/main/java/com/example/myjavaapplication/MainActivity.java
+++ app/src/main/java/com/example/myjavaapplication/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     private File photoFile;
 
     //private ImageView cropImage;
-
+    //
     private Uri imageCurrent;
 
     private ImageView imageView;
