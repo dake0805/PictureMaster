ba6d3aabc88849c2bfe5c649760746654b0200cb
zy <dake0805@gmail.com>
2019-09-02 14:48:11 +0800

    finish Brightness

--- app/src/main/java/com/example/myjavaapplication/PicColorControlActivity.java
+++ app/src/main/java/com/example/myjavaapplication/PicColorControlActivity.java
@@ -3,22 +3,31 @@ package com.example.myjavaapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
 import android.net.ProxyInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+
+import java.io.IOException;
 
 public class PicColorControlActivity extends AppCompatActivity {
 
 
     private ImageView imageView;
     private Uri imageUri;
+    private Bitmap bitmap;
 
     private static final int MAX_VALUE = 255;
     private static final int MID_VALUE = 127;
 
-    private double brightness;
+    private float brightness;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,11 +47,18 @@ public class PicColorControlActivity extends AppCompatActivity {
         seekBar.setMax(MAX_VALUE);
         seekBar.setProgress(MID_VALUE);
 
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 brightness = progress * 1.0f / MID_VALUE;
-                //do something
+                imageView.setImageBitmap(ChangeBrightness(bitmap, brightness));
             }
 
             @Override
@@ -57,5 +73,17 @@ public class PicColorControlActivity extends AppCompatActivity {
         });
     }
 
+
+    private static Bitmap ChangeBrightness(Bitmap startImage, float brightness) {
+        ColorMatrix lumMatrix = new ColorMatrix();
+        lumMatrix.setScale(brightness, brightness, brightness, 1);
+
+        Paint paint = new Paint();
+        paint.setColorFilter(new ColorMatrixColorFilter(lumMatrix));
+        Bitmap bitmap = Bitmap.createBitmap(startImage.getWidth(), startImage.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        canvas.drawBitmap(startImage, 0, 0, paint);
+        return bitmap;
+    }
 
 }
