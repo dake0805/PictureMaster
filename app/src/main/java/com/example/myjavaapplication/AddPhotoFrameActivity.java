227766f56155347ac698b78825de268d5ba45840
gyj234 <44019228+gyj234@users.noreply.github.com>
2019-09-04 10:59:03 +0800

    完善了AddPhotoFrameActivity，虚拟机运行可能报错，实机运行正常；
    TODO：AddPhotoFrameActivity中可能需要修改boarder值（设置边框大小）；
    activity_addphotoframe需要做页面布局调整。

--- app/src/main/java/com/example/myjavaapplication/AddPhotoFrameActivity.java
+++ app/src/main/java/com/example/myjavaapplication/AddPhotoFrameActivity.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import static android.graphics.Shader.TileMode.CLAMP;
 
@@ -22,6 +23,8 @@ public class AddPhotoFrameActivity extends Activity {
     private Button CircleBut;
     private Uri imageUri;
     private Bitmap bitmap;
+    private ImageView imageView;
+    private Uri AddFrame_Finish_uri;
 
     private int outWidth;
     private int outHeight;
@@ -33,11 +36,12 @@ public class AddPhotoFrameActivity extends Activity {
         setContentView(R.layout.activity_addphotoframe);
         RoundBut = (Button) findViewById(R.id.RoundBut);
         CircleBut = (Button) findViewById(R.id.CircleBut);
+        imageView = (ImageView) findViewById(R.id.imageview_addframe);
 
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
 
-        if (intent.getStringExtra("extra_addFrame") != null) {
-            imageUri = Uri.parse(intent.getStringExtra("extra_addFrame"));
+        if (intent.getStringExtra("extra_uri_process") != null) {
+            imageUri = Uri.parse(intent.getStringExtra("extra_uri_process"));
             try {
                 // 读取uri所在的图片
                 bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
@@ -45,6 +49,9 @@ public class AddPhotoFrameActivity extends Activity {
                 outHeight = bitmap.getHeight();
                 double rad = Math.sqrt(outWidth ^ 2 + outHeight ^ 2);
                 radius = new Double(rad).intValue();
+                imageView.setImageURI(imageUri);
+
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -53,14 +60,26 @@ public class AddPhotoFrameActivity extends Activity {
         RoundBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getRoundBitmapByShader(bitmap, outWidth, outHeight, radius, 1);
+                Bitmap backBitmap = getRoundBitmapByShader(bitmap, outWidth, outHeight, radius, 5);
+                AddFrame_Finish_uri = Uri.parse(MediaStore.Images.Media.insertImage(getContentResolver(), backBitmap, null, null));
+                imageView.setImageURI(AddFrame_Finish_uri);
+                //传回去
+                Intent intent = new Intent(AddPhotoFrameActivity.this, PictureProcessActivity.class);
+                intent.putExtra("extra_uri_process", AddFrame_Finish_uri.toString());
+                startActivity(intent);
             }
         });
 
         CircleBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getCircleBitmapByShader(bitmap, outWidth, outHeight, 1);
+                Bitmap backBitmap = getCircleBitmapByShader(bitmap, outWidth, outHeight, 1);
+                AddFrame_Finish_uri = Uri.parse(MediaStore.Images.Media.insertImage(getContentResolver(), backBitmap, null, null));
+                imageView.setImageURI(AddFrame_Finish_uri);
+                //传回去
+                Intent intent = new Intent(AddPhotoFrameActivity.this, PictureProcessActivity.class);
+                intent.putExtra("extra_uri_process", AddFrame_Finish_uri.toString());
+                startActivity(intent);
             }
         });
     }
@@ -108,9 +127,9 @@ public class AddPhotoFrameActivity extends Activity {
         canvas.drawRoundRect(rect, radius, radius, paint);
 
         if (boarder > 0) {
-            //绘制boarder
+            //绘制boarder 修改边框参数
             Paint boarderPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-            boarderPaint.setColor(Color.GREEN);
+            boarderPaint.setColor(Color.WHITE);
             boarderPaint.setStyle(Paint.Style.STROKE);
             boarderPaint.setStrokeWidth(boarder);
             canvas.drawRoundRect(rect, radius, radius, boarderPaint);
@@ -155,7 +174,7 @@ public class AddPhotoFrameActivity extends Activity {
         if (boarder > 0) {
             //绘制boarder
             Paint boarderPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-            boarderPaint.setColor(Color.GREEN);
+            boarderPaint.setColor(Color.WHITE);
             boarderPaint.setStyle(Paint.Style.STROKE);
             boarderPaint.setStrokeWidth(boarder);
             canvas.drawCircle(outWidth / 2, outHeight / 2, radius - boarder, boarderPaint);
