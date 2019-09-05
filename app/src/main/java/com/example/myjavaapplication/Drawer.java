646eb6af25888dfc493697d979d3bda4f918bb9e
ZhiYang <34023806+dake0805@users.noreply.github.com>
2019-09-05 22:26:58 +0800

    fix addText Layout && fix autoSave bug

--- app/src/main/java/com/example/myjavaapplication/Drawer.java
+++ app/src/main/java/com/example/myjavaapplication/Drawer.java
@@ -175,8 +175,14 @@ public class Drawer extends AppCompatActivity implements View.OnClickListener, M
                 Intent intent = new Intent(Drawer.this, PictureProcessActivity.class);
 
                 Bitmap bitmap = ImageUtils.createViewBitmap(picture, picture.getWidth(), picture.getHeight());
-                Uri Text_edit_Finsh_uri = Uri.parse(MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, null, null));
-                intent.putExtra("extra_uri_process", Text_edit_Finsh_uri.toString());
+                Uri Text_edit_Finish_uri = null;
+                BitmapToUri bitmapToUri = new BitmapToUri();
+                try {
+                    Text_edit_Finish_uri = bitmapToUri.BitmapToUri(bitmap, Drawer.this);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                intent.putExtra("extra_uri_process", Text_edit_Finish_uri.toString());
 
                 startActivity(intent);
 
